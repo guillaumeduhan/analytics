d8fcbfa994c7c531ec26b6fa2190fe402c4ea8a8
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2026-04-05 11:36:34 +0200

    pm2: force fork mode to avoid cluster double-bind

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "dist/main.js",
       cwd: "/home/pi/analytics/API",
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "256M",
